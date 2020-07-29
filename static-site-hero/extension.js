2b5128df4ea41432122f93637787ae3cff38f48d
Mike Leiter <light_cc@yahoo.com>
2020-07-28 21:28:28 -0400

    Add insertLink and insertFigure commands, along with keybindings to occur when an editor is in focus

--- static-site-hero/extension.js
+++ static-site-hero/extension.js
@@ -13,23 +13,36 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "static-site-hero" is now active!');
+	vscode.window.showWarningMessage("Warning!! Static Site Hero Helper is active...");
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('static-site-hero.helloWorld', function () {
+	let helloDisposable = vscode.commands.registerCommand('static-site-hero.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Static Site Hero!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(helloDisposable);
+
+	let fileLinkDisposable = vscode.commands.registerCommand('static-site-hero.insertLink', () => {
+		vscode.window.showInformationMessage('Insert File Link Initiated.');
+	});
+
+	context.subscriptions.push(fileLinkDisposable);
+
+	let figureDisposable = vscode.commands.registerCommand('static-site-hero.insertFigure', () => {
+		vscode.window.showInformationMessage('Insert Figure Tag Initiated');
+	});
+
+	context.subscriptions.push(figureDisposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
