3881456d9168b3307976df1e20bdf63d545b4092
Mike Leiter <light_cc@yahoo.com>
2020-07-28 20:14:14 -0400

    Update hello-world extension with minor updates - insert in editor, and error msg if no editor

--- hello-world/extension.js
+++ hello-world/extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { toEditorSettings } = require('typescript');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +18,34 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('hello-world.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('hello-world.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Hello World!');
+		// vscode.window.showInformationMessage('Hello World from Hello World!');
+
+		var editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			vscode.window.showErrorMessage("No file is open, can't say Hello!");
+			return;
+		}
+
+		// toEditorSettings.edit(function (editBuilder) {
+		// 	editBuilder.insert(editor.selection.start, "Insert At Start!!");
+		// });
+
+		try {
+			editor.edit((editBuilder) => {
+				editBuilder.delete(editor.selection);
+			}).then((editBuilder) => {
+				editor.edit((editBuilder) => {
+					editBuilder.insert(editor.selection.start, "Delete and Insert - Hello World!!");
+				});
+			});
+		} catch (ex) {
+			console.error("Error writing to doc." + ex);
+		}
 	});
 
 	context.subscriptions.push(disposable);
@@ -29,7 +53,7 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
